14e924f668c5d2632a384d90e0f8b9a4f8c0b048
Ashish Chaudhary <grunch.me1@gmail.com>
2026-05-29 22:06:48 +0545

    chore: update dynamic dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,98 @@
+from datetime import date
+
 from django.shortcuts import render
+
+# https://placehold.co/320x240
+all_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "https://placehold.co/320x240",
+        "author": "Maximilian",
+        "date": date(2021, 7, 21),
+        "title": "consectetur adipisicing elit",
+        "excerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit officiis nobisaperiam est praesentium,!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "https://placehold.co/320x240",
+        "author": "Maximilian",
+        "date": date(2022, 3, 10),
+        "title": "quos iste consequuntur omnis!",
+        "excerpt": "quos iste consequuntur omnis exercitationem quamvelit labore vero culpa ad mollitia?...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "https://placehold.co/320x240",
+        "author": "Maximilian",
+        "date": date(2020, 8, 5),
+        "title": "Lorem ipsum dolor sit amet",
+        "excerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
+]
+
+
+def get_date(post):
+    return post['date']
 
 # Create your views here.
 
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_post = sorted_posts[-3:]
+
+    return render(request, "blog/index.html", {
+        "posts": latest_post
+    })
 
 
 def posts(request):
-    return render(request, "blog/all-post.html")
+    return render(request, "blog/all-post.html", {
+        "all_posts": all_posts
+    })
 
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post
+    })
