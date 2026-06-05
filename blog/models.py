a10e4539392ed0a37944786c9601fdd86073c1bc
Ashish Chaudhary <grunch.me1@gmail.com>
2026-06-05 22:11:12 +0545

    feat: add admin filter and auto-generate slug by title

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,9 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.caption
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
@@ -29,4 +32,4 @@ class Post(models.Model):
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(
         Author, on_delete=models.SET_NULL, null=True, related_name="posts")
-    tag = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag)
