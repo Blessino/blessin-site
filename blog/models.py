3b28ae02638900f1dbee51050922045f54cf90d1
Ashish Chaudhary <grunch.me1@gmail.com>
2026-06-18 20:41:06 +0545

    feat: render comment form on post detail page

--- blog/models.py
+++ blog/models.py
@@ -37,7 +37,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     user_name = models.CharField(max_length=120)
-    email = models.EmailField()
+    user_email = models.EmailField()
     text = models.TextField(max_length=400)
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comment")
